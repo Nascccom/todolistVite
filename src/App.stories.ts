6b756c93619ba0b426668802327b1893c65352d2
Nascccom <ostrov_o@bk.ru>
2023-04-24 16:47:27 +0300

    Created App.stories

---
+++ src/App.stories.ts
@@ -0,0 +1,24 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {ReduxStoreDecorator} from "./store/ReduxStoreDecorator/ReduxStoreDecorator";
+import App from "./App";
+
+
+const meta: Meta<typeof App> = {
+    title: 'TODOLISTS/App',
+    component: App,
+    tags: ['autodocs'],
+    decorators: [ReduxStoreDecorator],
+
+}
+
+export default meta;
+type Story = StoryObj<typeof App>;
+
+export const AppWithTodolists: Story = {
+
+}
+
+
+
+
+
