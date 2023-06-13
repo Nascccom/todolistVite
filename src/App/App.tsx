733e5112ab22b54b5c7a6accd6fcaa3427383ca4
Nascccom <ostrov_o@bk.ru>
2023-06-13 11:11:53 +0300

    Refactoring

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,10 @@
-import React, {useCallback, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import './App.css'
 import Container from '@mui/system/Container';
 import {ButtonAppBar} from "../components/ButtonAppBar/ButtonAppBar";
-import Grid from '@mui/material/Grid';
-import {InputLine} from "../components/InputLine/InputLine";
 import {useAppDispatch} from "../hooks/useDispatch/useDispatch";
-import {addTodolistTC, getTodolistsTC} from "../store/reducers/todolistReducer/todolists-reducer";
-import {TodolistsList} from "../components/TodoListsList/TodoListsList";
+import {getTodolistsTC} from "../store/reducers/todolistReducer/todolists-reducer";
+import {TodolistsList} from "../components/TodolistsList/TodolistsList";
 
 function App() {
     const dispatch = useAppDispatch()
@@ -15,29 +13,17 @@ function App() {
         dispatch(getTodolistsTC())
     }, [])
 
-    const addTodolist = useCallback((title: string) => {
-        dispatch(addTodolistTC(title))
-    }, [dispatch])
-
     return (
       <div className="App">
           <ButtonAppBar/>
 
           <Container fixed>
-              <Grid container sx={{justifyContent: 'center', marginTop: "30px"}}>
-                  <InputLine callBack={addTodolist}/>
-              </Grid>
-              <Grid container spacing={5} sx={{justifyContent: 'center', marginTop: "20px"}}>
-
-                  <TodolistsList/>
-
-              </Grid>
+              <TodolistsList/>
           </Container>
 
       </div>
     )
 }
-
 
 
 export default App
---
+++ src/components/TodolistsList/TodolistsList.tsx
@@ -0,0 +1,41 @@
+import {useAppSelector} from "../../hooks/useSelector/useSelector";
+import {addTodolistTC, TodolistDomainType} from "../../store/reducers/todolistReducer/todolists-reducer";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import {Todolist} from "../Todolist/Todolist";
+import {InputLine} from "../InputLine/InputLine";
+import React, {useCallback} from "react";
+import {useAppDispatch} from "../../hooks/useDispatch/useDispatch";
+
+export const TodolistsList = () => {
+    const todolists = useAppSelector<TodolistDomainType[]>(state => state.todolists)
+    const dispatch = useAppDispatch()
+
+    const addTodolist = useCallback((title: string) => {
+        dispatch(addTodolistTC(title))
+    }, [dispatch])
+
+    return (
+      <>
+          <Grid container sx={{justifyContent: 'center', marginTop: "30px"}}>
+              <InputLine callBack={addTodolist}/>
+          </Grid>
+
+          <Grid container spacing={5} sx={{justifyContent: 'center', marginTop: "20px"}}>
+              {todolists.map(todo => {
+                  return (
+                    <Grid item key={todo.id}>
+                        <Paper style={{padding: '10px'}}>
+
+                            <Todolist todolistId={todo.id}
+                                      title={todo.title}
+                                      activeFilter={todo.filter}/>
+
+                        </Paper>
+                    </Grid>
+                  )
+              })}
+          </Grid>
+      </>
+    )
+}
